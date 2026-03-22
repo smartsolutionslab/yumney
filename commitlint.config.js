e812805ef773777aba50044135971d545469554a
Heiko Weiss <heiko@smartsolutionslab.tech>
2026-03-22 01:37:31 +0100

    chore(ci): add missing scopes and increase header-max-length to 120

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,8 +4,9 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      ['recipes', 'shopping', 'users', 'account', 'shared', 'api', 'shell', 'infra', 'ui', 'scaffold', 'ci', 'auth', 'e2e'],
+      ['recipes', 'shopping', 'users', 'account', 'shared', 'api', 'shell', 'infra', 'ui', 'scaffold', 'ci', 'auth', 'e2e', 'frontend', 'test', 'domain', 'application'],
     ],
+    'header-max-length': [2, 'always', 120],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'body-max-line-length': [1, 'always', 120],
   },
