44e844a086bd9dd41c7c2a3c7c68581c05956bc1
Heiko Weiss <heiko@smartsolutionslab.tech>
2026-03-25 10:09:08 +0100

    style(shopping): apply linter refactoring to DI registration (US-000)

--- src/Yumney.Shopping.Infrastructure/ShoppingInfrastructureServiceCollectionExtensions.cs
+++ src/Yumney.Shopping.Infrastructure/ShoppingInfrastructureServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using SmartSolutionsLab.Yumney.Shared.Persistence;
 using SmartSolutionsLab.Yumney.Shopping.Domain.ShoppingList;
 using SmartSolutionsLab.Yumney.Shopping.Infrastructure.Persistence;
@@ -12,11 +13,14 @@ public static class ShoppingInfrastructureServiceCollectionExtensions
     public static IServiceCollection AddShoppingInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ShoppingDbContext>((sp, options) =>
+        {
+            var connectionString = configuration.GetConnectionString("shoppingdb");
+            Action<NpgsqlDbContextOptionsBuilder> contextOptions = builder => builder.MigrationsHistoryTable("__ShoppingMigrationsHistory");
+
             options
-                .UseNpgsql(
-                    configuration.GetConnectionString("shoppingdb"),
-                    x => x.MigrationsHistoryTable("__ShoppingMigrationsHistory"))
-                .AddInterceptors(sp.GetRequiredService<DomainEventDispatchInterceptor>()));
+                .UseNpgsql(connectionString, contextOptions)
+                .AddInterceptors(sp.GetRequiredService<DomainEventDispatchInterceptor>());
+        });
 
         services.AddScoped<IShoppingListRepository, ShoppingListRepository>();
         services.AddHealthChecks().AddDbContextCheck<ShoppingDbContext>("shoppingdb");
