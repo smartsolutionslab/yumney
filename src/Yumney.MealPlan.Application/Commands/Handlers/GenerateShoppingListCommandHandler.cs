4d6a631569cecabfc20545f4e17c0e6b6495a5ff
Heiko Weiss <heiko@smartsolutionslab.tech>
2026-04-18 14:31:26 +0200

    refactor(mealplan): move GenerateShoppingListResultDto to DTOs folder (US-000)

--- src/Yumney.MealPlan.Application/Commands/Handlers/GenerateShoppingListCommandHandler.cs
+++ src/Yumney.MealPlan.Application/Commands/Handlers/GenerateShoppingListCommandHandler.cs
@@ -1,3 +1,4 @@
+using SmartSolutionsLab.Yumney.MealPlan.Application.DTOs;
 using SmartSolutionsLab.Yumney.MealPlan.Domain.WeeklyPlan;
 using SmartSolutionsLab.Yumney.Shared.Common;
 using SmartSolutionsLab.Yumney.Shared.CQRS;
