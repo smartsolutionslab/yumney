0497eccfb0816666119ea41ea3f1aa7027353afb
Heiko Weiss <heiko@smartsolutionslab.tech>
2026-04-18 14:31:26 +0200

    refactor(mealplan): move GenerateShoppingListResultDto to DTOs folder (US-000)

--- src/Yumney.MealPlan.Application/DTOs/GenerateShoppingListResultDto.cs
+++ src/Yumney.MealPlan.Application/DTOs/GenerateShoppingListResultDto.cs
@@ -1,3 +1,3 @@
-namespace SmartSolutionsLab.Yumney.MealPlan.Application.Commands;
+namespace SmartSolutionsLab.Yumney.MealPlan.Application.DTOs;
 
 public sealed record GenerateShoppingListResultDto(int ItemsAdded, int StaplesSkipped);
