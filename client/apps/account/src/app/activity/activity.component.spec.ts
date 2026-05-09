9048e815a88023bb52c8471d5aaaf5162c4ff7c8
Heiko Weiss <heiko@smartsolutionslab.tech>
2026-05-09 23:15:10 +0200

    style(account): collapse multi-line @yumney/ui import to satisfy prettier

--- client/apps/account/src/app/activity/activity.component.spec.ts
+++ client/apps/account/src/app/activity/activity.component.spec.ts
@@ -1,11 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
-import {
-  ActivityTimelineComponent,
-  AsyncStateComponent,
-  provideYumneyIcons,
-} from '@yumney/ui';
+import { ActivityTimelineComponent, AsyncStateComponent, provideYumneyIcons } from '@yumney/ui';
 import { setupTranslocoTesting } from '@yumney/shared/models';
 import { ActivityComponent } from './activity.component';
 import { ActivityApiService, type UserActivityPage } from '../api';
