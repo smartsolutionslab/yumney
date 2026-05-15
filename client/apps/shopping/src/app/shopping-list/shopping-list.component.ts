31d8acc7e3548dffcbeda74bd69cfc1bd27ddeb1
Heiko Weiss <heiko@smartsolutionslab.tech>
2026-05-15 10:52:44 +0200

    style(frontend): prettier format

--- client/apps/shopping/src/app/shopping-list/shopping-list.component.ts
+++ client/apps/shopping/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  inject,
-  OnInit,
-  signal,
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { TranslocoModule } from '@jsverse/transloco';
